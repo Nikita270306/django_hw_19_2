74bbe8fb48f7e1d50c4286df899a72e058684662
nikita <n.shavkin@it-park.tech>
2024-01-22 10:23:19 +0300

    added

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,5 @@ from catalog.views import index, home, contact
 urlpatterns = [
     path('', index),
     path('home/', home, name='home'),
-    path('contact/', contact, name='contact'),
+    path('contacts/', contact, name='contact'),
 ]
