9ff4455949a1cc48c17cec08add2a9cc23a12240
nikita <n.shavkin@it-park.tech>
2024-02-13 15:16:29 +0300

    .

--- catalog/management/commands/fill_prod.py
+++ catalog/management/commands/fill_prod.py
@@ -1,18 +1,19 @@
 import json
 from django.core.management import BaseCommand
-from catalog.models import Product
+from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
+
     def handle(self, *args, **options):
         Product.objects.all().delete()
 
-        products_list = []
         with open('catalog/fixtures/product.json', encoding='utf-8') as f:
-            dict_prod = json.load(f)
+            products_data = json.load(f)
 
-        for product in dict_prod:
-            products_list.append(Product(name=product['fields']['name'], description=product['fields']['description']))
-
-        Product.objects.bulk_create(products_list)
-
+        # Создание категорий и продуктов
+        for product_item in products_data:
+            category_id = product_item["fields"].pop("category")  # Получаем и удаляем идентификатор категории из данных продукта
+            category = Category.objects.get(pk=category_id)  # Получаем объект категории по идентификатору
+            product_item["fields"]["category"] = category  # Заменяем идентификатор категории объектом категории
+            Product.objects.create(**product_item["fields"])
