3c524004a1c9edaec00242cf72713e5488953470
nikita <n.shavkin@it-park.tech>
2024-05-21 15:40:02 +0300

    -

--- django_hw_19_2/settings.py
+++ django_hw_19_2/settings.py
@@ -123,6 +123,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- user/views.py
+++ user/views.py
@@ -2,11 +2,17 @@ from django.conf import settings
 from django.contrib.auth import login
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.views import PasswordResetDoneView, PasswordResetConfirmView, \
     PasswordResetCompleteView, LogoutView
 from django.core.mail import send_mail
-from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.template.context_processors import request
+from django.urls import reverse_lazy, reverse
 from django.utils.crypto import get_random_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.views.generic import CreateView, FormView, TemplateView
 from .forms import CustomUserCreationForm
 from .models import CustomUser
@@ -26,7 +32,6 @@ class RegisterView(CreateView):
 
         return response
 
-
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('catalog:home')
 
@@ -34,6 +39,31 @@ class CustomLogoutView(LogoutView):
         return super().post(request, *args, **kwargs)
 
 
+# class UserPasswordResetView(FormView):
+#     template_name = 'registration/password_reset_email.html'
+#     form_class = PasswordResetForm
+#     success_url = reverse_lazy('registration/:change_password.html')
+#
+#     def form_valid(self, form):
+#         email = form.cleaned_data['email']
+#         new_password = get_random_string(length=12)
+#
+#         try:
+#             user = CustomUser.objects.get(email=email)
+#             user.set_password(new_password)
+#             user.save()
+#
+#             send_mail(
+#                 subject='Password Reset Request',
+#                 message=f'Your new password is: {new_password}',
+#                 from_email=settings.EMAIL_HOST_USER,
+#                 recipient_list=[email]
+#             )
+#
+#             return render(request, 'registration/change_password.html')
+#         except User.DoseNotExist:
+#             return render(request, 'registration/password_reset_email.html', "error: 'Не получилось!'")
+# #
 class UserPasswordResetView(FormView):
     template_name = 'registration/password_form.html'
     form_class = PasswordResetForm
